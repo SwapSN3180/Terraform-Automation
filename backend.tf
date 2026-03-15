137aa0fac559542f932d2c1f29fefcccc2df82fc
SwapSN3180 <134468261+SwapSN3180@users.noreply.github.com>
2026-03-15 14:44:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state-DevopsProject1"
+    bucket = "young-minds-app-project-terraform-state-devopsproject1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
